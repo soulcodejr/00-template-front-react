3af2aa7e089929ff497ffbd4194e377869e76251
Ana Caroline Ramos Klen <juliana.rkg544@gmail.com>
2024-07-25 14:28:35 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from "./components/App"
+import "./assets/css/reset.css"
+import "./assets/css/index.css"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
